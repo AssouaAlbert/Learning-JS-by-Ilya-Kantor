b8f9f8cf29ad375fa2c7c686b89ba30bdf721785
AssouaAlbert <assouaalbert@gmail.com>
2020-02-29 04:58:32 +0300

    Sorry VS code I have been using thedebiger all this while ;)

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let i = 0;
+let anotherI = 'Just Another Variable';
 function hello(name) {
     let phrase = `Hello, ${name}!`;
     debugger; // <-- the debugger stops here
     say(phrase);
     }
+function say(m){
+    alert(m)
+}
